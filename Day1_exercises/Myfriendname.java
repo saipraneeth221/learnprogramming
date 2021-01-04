48aa7cb81bf364bef27d8544c6015d1c18ec1b40
saipraneeth <praneethsai221@gmail.com>
2021-01-04 20:37:23 +0530

    day1_exercises

---
+++ Day1_exercises/Myfriendname.java
@@ -0,0 +1,9 @@
+package org.com.pra;
+
+public class Myfriendname {
+public static void main(String[]args) {
+	System.out.print("Myname:k.sai praneeth reddy");
+	System.out.print("\nMyfriendname:k.manasa");
+	
+}
+}
