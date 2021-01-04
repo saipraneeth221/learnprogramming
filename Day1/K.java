c064978f6dafc8d30b4ee776c198940954068027
saipraneeth <praneethsai221@gmail.com>
2021-01-04 20:24:33 +0530

    Day1

---
+++ Day1/K.java
@@ -0,0 +1,15 @@
+package org.com.pra;
+
+public class K {
+	public static void main(String[]args) {
+		System.out.println("k     k");
+		System.out.println("k   k  ");
+		System.out.println("k k    ");
+		System.out.println("k      ");
+		System.out.println("k k    ");
+		System.out.println("k   k  ");
+		System.out.println("k     k");
+		
+	}
+
+}
---
+++ Day1/Myaddress.java
@@ -0,0 +1,8 @@
+package org.com.pra;
+
+public class Myaddress {
+	public static void main(String[]args) {
+		System.out.println("k.sai praneeth reddy, \nS/o k.bhaskar reddy,\nChinmaya nagar,\npincode 515002");
+	}
+
+}
